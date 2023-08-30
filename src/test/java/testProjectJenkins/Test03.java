af8393b4a5c9e1c930583c321a57d2e5c26681bc
chitra <chitralumia1@gmail.com>
2023-08-30 21:45:46 +0530

    First Commit

---
+++ src/test/java/testProjectJenkins/Test03.java
@@ -0,0 +1,15 @@
+package testProjectJenkins;
+
+public class Test03 
+{
+	public static void main(String args[])
+	{
+			int a = 10;
+			int b = 20;
+	
+			System.out.println("Sum is" + a+b);
+	}
+	
+	
+	
+}
